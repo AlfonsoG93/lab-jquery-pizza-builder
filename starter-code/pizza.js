ef6181eb7f7c0bca2833e31434da55cb74e830ee
Alfonso Moncada <alfonsomcd93@gmail.com>
2017-10-30 10:13:49 -0400

    Buttons sectoin pizza

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
 // Write your Pizza Builder JavaScript in this file.
+var  totalPrice ;
+$(document).ready(function(){
+
+$('.btn-pepperonni').click(function(){
+    $('.pep').toggle();
+    $('.btn-pepperonni').toggleClass('active');
+    
+});
+$('.btn-green-peppers').click(function(){
+    $('.green-pepper').toggle();
+    $('.btn-green-peppers').toggleClass('active');
+    
+});
+
+$('.btn-mushrooms').click(function(){
+    $('.mushroom').toggle();
+    $('.btn-mushroom').toggleClass('active');
+});
+
+$('.sauce').removeClass('sauce-white');
+$('.btn-sauce').click(function(){
+    $('.sauce').toggleClass('sauce-white');
+    $('.sauce').toggleClass('active');
+});
+
+$('.crust').removeClass('crust-gluten-free');
+$('.btn-crust').click(function(){
+    $('.crust').toggleClass('crust-gluten-free');
+    $('.btn-crust').toggleClass('active');
+});
+
+});
